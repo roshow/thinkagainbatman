8bd7f162deba2702aa1bd96c2ac53c7c190f4f86
roshow <rolando@roshow.net>
2014-04-05 14:35:14 -0400

    much better basics with regex

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var conf = {
     DB_FULLPATH: 'ds043388.mongolab.com:43388/thinkbatman'
 };
 
-console.log('mongodb://' + conf.DB_USER + ':' + conf.DB_PW +  '@' + conf.DB_FULLPATH);
-
 server
   .use(restify.fullResponse())
   .use(restify.bodyParser());
@@ -18,10 +16,9 @@ server
 handler.connectDb('mongodb://' + conf.DB_USER + ':' + conf.DB_PW +  '@' + conf.DB_FULLPATH)
     .then(function(){
         /** Go get some! **/
-        server.get('/thought', handler.thought.get);
-        server.get('/thought/:batId', handler.thought.get);
+        server.get(/^\/(?:bat-|)thought(?:\/(.*)|)$/, handler.thought.get);
         /** Put some over here **/
-        server.put('/thought', handler.thought.put);
+        server.put(/^\/(?:bat-|)thought(?:\/|)$/, handler.thought.put);
         /** I'm here to listen **/
         server.listen(7777, function(){
             console.log('%s listening at %s', server.name, server.url);
