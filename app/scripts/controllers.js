4bdc6d022396374852af8df08a6557827bac3f30
roshow <rolando@roshow.net>
2014-04-15 03:44:00 -0400

    something

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,19 +1,22 @@
 'use strict';
-// var AYE = 0;
+
 angular.module('thinkagainbatmanApp')
     .controller('ThoughtCtrl', ['$scope', '$routeParams', '$location', 'GetAThought',
         function ($scope, $routeParams, $location, GetAThought) {
-            $scope.getRandom = function(){
-                // console.log('get a random thought '+ (AYE++));
+            console.log($routeParams);
+            function getRandom(){
                 GetAThought.random(function(thought){
                     $location.path('thought/' + thought.id);
                 });
             };
+            $scope.getRandom = getRandom;
             if ($routeParams.batId){
-                var q = ($routeParams.batId.length === 24) ? { _id: $routeParams.batId } : { id: $routeParams.batId };
+                var qKey = ($routeParams.batId.length === 24) ? '_id' : 'id',
+                    q = {};
+                q[qKey] = $routeParams.batId;
                 $scope.thought = GetAThought.query(q);
             }
             else {
-                $scope.getRandom();
+                getRandom();
             }
         }]);
