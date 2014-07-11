bb1a75254fa2e24e6ad9b3698481bb05eac8fe6b
roshow <rolando@roshow.net>
2014-07-11 00:03:36 -0400

    dev matches manage without manage

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -1,28 +1,39 @@
 'use strict';
 
 angular.module('thinkagainbatmanApp')
-    .directive('backImg', function(){
-        return function(scope, element, attrs){
-            attrs.$observe('backImg', function(value) {
-                var i = Math.floor(Math.random()*3),
-                    img = new Image();
-                element.html('<img id="batspinner" src="http://roshow.net/public/images/thinkbatman/spinners/batspinner_'+ i +'.jpg">');
-                img.onload = function(){
-                    element.html('');
-                    element.css({
-                        'background-image': 'url(' + value +')'
-                    });
-                };
-                img.src = value;
+
+.directive('backImg', function(){
+    return {
+        restrict: 'A',
+        scope: {
+            backImg: '='
+        },
+        link: function(scope, element, attr){
+            scope.$watch('backImg', function (newVal, oldVal){
+                if (newVal && newVal.src){
+                    var i = Math.floor(Math.random()*3),
+                        img = new Image();
+                    // Spinner
+                    // element.html('<img id="batspinner" src="http://roshow.net/public/images/thinkbatman/spinners/batspinner_'+ i +'.jpg">');
+                    img.onload = function(){
+                        element.html('');
+                        element.css({
+                            'background-image': 'url(' + newVal.src +')',
+                            'background-size' : newVal.scale
+                        });
+                    };
+                    img.src = newVal.src;
+                }
             });
-        };
-    })
-    .directive('backSize', function(){
-        return function(scope, element, attrs){
-            attrs.$observe('backSize', function(value) {
-                element.css({
-                    'background-size' : value
-                });
-            });
-        };
-    });
+        }
+    };
+})
+
+.directive('linkTo', ['$location', function($location){
+    return function(scope, element, attr){
+        element.on('click', function(){
+            $location.path(attr.linkTo).replace();
+            scope.$apply();
+        });
+    };
+}]);
