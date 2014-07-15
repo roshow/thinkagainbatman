66fa9aaf024f633a667176d388a84524ea457702
roshow <rolando@roshow.net>
2014-07-14 23:45:04 -0400

    reset ajax calls to 0.0.0.0:5000

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,7 +2,7 @@
 
 angular.module('thinkagainbatmanApp')
     .factory('GetAThought', ['$resource', function($resource){
-        return $resource('/thought/:id', {}, {
+        return $resource('http://0.0.0.0:5000/thought/:id', {}, {
             query: {
                 method: 'GET',
                 isArray: false
