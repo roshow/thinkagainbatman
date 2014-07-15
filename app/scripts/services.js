6d6fca5278aee66e37e078137f91f93d0713450e
roshow <rolando@roshow.net>
2014-07-14 21:53:33 -0400

    ready for dist

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,7 +2,7 @@
 
 angular.module('thinkagainbatmanApp')
     .factory('GetAThought', ['$resource', function($resource){
-        return $resource('http://0.0.0.0:5000/thought/:id', {}, {
+        return $resource('/thought/:id', {}, {
             query: {
                 method: 'GET',
                 isArray: false
